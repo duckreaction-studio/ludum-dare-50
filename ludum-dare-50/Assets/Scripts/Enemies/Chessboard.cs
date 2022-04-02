251782f169b997dd61b1265b2ac4d9912facdd7d
Matthieu Giraud <matthieu.giraud99@gmail.com>
2022-04-02 11:44:56 +0200

    Creation du chessboard

--- ludum-dare-50/Assets/Scripts/Enemies/Chessboard.cs
+++ ludum-dare-50/Assets/Scripts/Enemies/Chessboard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,25 @@ namespace Enemies
     {
         public static readonly int size = 8;
 
-        [SerializeField] private float _squareSize;
+        [SerializeField] private float _squareSize = 1f;
+        [SerializeField] private float _gizmoLineLength = 1f;
+
+        private void OnDrawGizmos()
+        {
+            for (var column = 0; column < size; column++)
+            {
+                for (var row = 0; row < size; row++)
+                {
+                    var position = GetSquareWorldPosition(column, row);
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawLine(position, position + Vector3.up * _gizmoLineLength);
+                }
+            }
+        }
+
+        private Vector3 GetSquareWorldPosition(int column, int row)
+        {
+            return transform.position + Vector3.right * _squareSize * column + Vector3.forward * _squareSize * row;
+        }
     }
 }
