7294848956844c0cc5d4f481db213d63beb206e4
Matthieu Giraud <matthieu.giraud99@gmail.com>
2022-04-02 11:24:19 +0200

    Mise en place scene de test

---
+++ ludum-dare-50/Assets/Scripts/Enemies/Chessboard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class Chessboard : MonoBehaviour
+    {
+        public static readonly int size = 8;
+
+        [SerializeField] private float _squareSize;
+    }
+}
