41ef3eae364a1a4898caef5024b5a6e4b9d9a638
Matthieu Giraud <matthieu.giraud99@gmail.com>
2022-04-03 18:33:38 +0200

    Tutorial WIP

--- ludum-dare-50/Assets/Scripts/LevelInstaller.cs
+++ ludum-dare-50/Assets/Scripts/LevelInstaller.cs
@@ -7,6 +7,7 @@ public class LevelInstaller : MonoInstaller
     [Preserve]
     public override void InstallBindings()
     {
-       
+        Debug.Log("Install level");
+        Container.Bind<Player>().FromComponentInHierarchy(false).AsSingle();
     }
 }
