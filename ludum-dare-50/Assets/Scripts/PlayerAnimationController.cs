3571774e868c91ae8ad8f3098352a9bbf64c8291
Matthieu Giraud <matthieu.giraud99@gmail.com>
2022-04-04 02:22:54 +0200

    Reset du hero + ui WIP

--- ludum-dare-50/Assets/Scripts/PlayerAnimationController.cs
+++ ludum-dare-50/Assets/Scripts/PlayerAnimationController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using DuckReaction.Common;
 using UnityEngine;
 using Zenject;
 
@@ -8,7 +9,28 @@ public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera _aimingCamera;
     [Inject] Player _player;
+    [Inject] SignalBus _signalBus;
 
+    Vector3 _startPosition;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+        _signalBus.Subscribe<GameEvent>(OnGameEventReceived);
+    }
+
+    void OnGameEventReceived(GameEvent gameEvent)
+    {
+        if (gameEvent.Is(GameEventType.LevelRestart))
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        transform.position = _startPosition;
+    }
 
     void Update()
     {
